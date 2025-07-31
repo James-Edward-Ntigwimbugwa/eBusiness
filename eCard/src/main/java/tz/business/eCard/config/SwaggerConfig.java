1551cd1cc6048fb1ea43d9125b1a5a050129d0fb
James Edward <jamesnavigator04@gmail.com>
2025-07-31 03:49:46 +0300

    config updatess

--- eCard/src/main/java/tz/business/eCard/config/SwaggerConfig.java
+++ eCard/src/main/java/tz/business/eCard/config/SwaggerConfig.java
@@ -1,29 +1,16 @@
 package tz.business.eCard.config;
 
 import io.swagger.v3.oas.annotations.OpenAPIDefinition;
-import io.swagger.v3.oas.annotations.enums.SecuritySchemeType;
-import io.swagger.v3.oas.annotations.info.Contact;
 import io.swagger.v3.oas.annotations.info.Info;
-import io.swagger.v3.oas.annotations.info.License;
-import io.swagger.v3.oas.annotations.security.SecurityScheme;
 import io.swagger.v3.oas.annotations.servers.Server;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
 @OpenAPIDefinition(
-        servers = { @Server(url = "http://192.168.1.150:8080") },
-        info = @Info(title = "FEEDBACK PLATFORM API",
-                version = "v1",
-                description = "FEEDBACK PLATFORM RESTful API",
-                license = @License(
-                        name = "MIT License"),
-                contact = @Contact(url = "a",
-                        name = "James Edward")))
-@SecurityScheme(
-        name = "Bearer Authentication",
-        type = SecuritySchemeType.HTTP,
-        bearerFormat = "JWT",
-        scheme = "bearer"
+        info = @Info(title = "Sims API", version = "1.0"),
+        servers = {
+                @Server(url = "http://localhost:8080", description = "Local server")
+        }
 )
 public class SwaggerConfig {
+    // Additional configuration if needed
 }
+
